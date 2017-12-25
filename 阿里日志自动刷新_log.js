d46a89a7ce49d9a4aab063ffcfa3275f94eda167
vick <lianghuiming@luojilab.com>
2017-12-25 16:38:48 +0800

    键盘监听

--- 阿里日志自动刷新_log.js
+++ 阿里日志自动刷新_log.js
@@ -14,10 +14,40 @@
     // Your code here...
    var refresh_btn = $(".next-btn-medium.next-btn-primary")[0];
    function refreash_(){
-       refresh_btn.click();
+       //refresh_btn.click();
    }
    var interid = setInterval(refreash_, 1000);
     console.log("interval ID is ", interid);
+
+
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+      
+        if (keyName === 'Control') {
+          // do not alert when only Control key is pressed.
+          return;
+        }
+      
+        if (event.ctrlKey) {
+          // Even though event.key is not 'Control' (i.e. 'a' is pressed),
+          // event.ctrlKey may be true if Ctrl key is pressed at the time.
+          alert(`Combination of ctrlKey + ${keyName}`);
+        } else {
+          alert(`Key pressed ${keyName}`);
+        }
+      }, false);
+      
+      document.addEventListener('keyup', (event) => {
+        const keyName = event.key;
+      
+        // As the user release the Ctrl key, the key is no longer active.
+        // So event.ctrlKey is false.
+        if (keyName === 'Control') {
+          alert('Control key was released');
+        }
+      }, false);
+
+
 
 
 })();
