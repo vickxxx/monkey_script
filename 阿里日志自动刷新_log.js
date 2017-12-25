7d11c4839498542046a80220dfdf9d5376cb720b
vick <lianghuiming@luojilab.com>
2017-12-25 14:18:54 +0800

    a阿里日志刷新

---
+++ 阿里日志自动刷新_log.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         python阿里日志刷新
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  定时刷新阿里日志输出
+// @author       You
+// @match        https://sls.console.aliyun.com/next/project/luojilab-logs/logsearch/passport-python*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+   var refresh_btn = $(".next-btn-medium.next-btn-primary")[0];
+   function refreash_(){
+       refresh_btn.click();
+   }
+   var interid = setInterval(refreash_, 1000);
+    console.log("interval ID is ", interid);
+
+
+})();
